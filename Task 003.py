4940b21ba45b27561da18ae48cd380d0ddd44493
Abhishek Gope <105343069+ABHIGOPE@users.noreply.github.com>
2024-03-10 21:45:24 +0530

    Update Task 003.py

--- Task 003.py
+++ Task 003.py
@@ -1,10 +1,10 @@
-#Task 1
+#Task 1(User Input)
 name=(input("Enter your name \n"))
 print(name)
 
-#Task 2
+#Task 2 (User Input with message)
 name=(input("Enter your name \n"))
 print(name,"Welcome to the python class")
 
-#Task 3
+#Task 3 (Name printing with a seperator
 print("Abhishek","Gope",sep="_")
