7bf1f50cc56966b1a3ebb2f83127cbabd70f5d59
Abhishek Gope <105343069+ABHIGOPE@users.noreply.github.com>
2024-03-10 21:22:24 +0530

    Add files via upload

---
+++ Task 003.py
@@ -0,0 +1,10 @@
+#Task 1
+name=(input("Enter your name \n"))
+print(name)
+
+#Task 2
+name=(input("Enter your name \n"))
+print(name,"Welcome to the python class")
+
+#Task 3
+print("Abhishek","Gope",sep="_")
