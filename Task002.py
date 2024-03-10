872b2f605e4830e3fc42c8b5bd2bc36c6f87edc8
Abhishek Gope <105343069+ABHIGOPE@users.noreply.github.com>
2024-03-10 21:21:47 +0530

    Add files via upload

---
+++ Task002.py
@@ -0,0 +1,26 @@
+#Keyword
+
+import keyword
+print(keyword.kwlist)
+
+name= "class"
+print(name)
+
+#Variables---
+name="abhi"
+print(name)
+name2="gope"
+print(name2)
+
+#Data types
+
+print(type('x'))
+print(type(name))
+pi=3.14
+print(pi)
+name="abhi"
+isMale=True
+print(type(isMale))
+a=3
+cn=a+4j
+print(type(cn))
